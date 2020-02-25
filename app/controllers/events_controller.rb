abfa14d7450509c1c568f5c036a0a1831499ae25
lluisguardiola <lluisguardiola.h@gmail.com>
2020-02-25 12:17:22 -0600

    added event show route, fixed event attendees association

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,4 +2,9 @@ class EventsController < ApplicationController
 	def index
 		render json: Event.all
 	end
+
+	def show
+		event = Event.find(params[:id])
+		render json: event, include: [:game, :host, :attendees]
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
     resources :users, only: [:index, :create]
-    resources :events, only: [:index]
+    resources :events, only: [:index, :show]
     
     post '/auth', to: 'auth#create'
     
