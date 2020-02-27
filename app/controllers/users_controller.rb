70358f97e080cf193d3ab20526b821546a74075d
lluisguardiola <lluisguardiola.h@gmail.com>
2020-02-27 17:03:23 -0600

    users show controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,32 @@
 class UsersController < ApplicationController
-  def index
-	render json: User.all
-  end
-
-  def create
-	user = User.new(
-		username: params[:username],
-		password: params[:password], 
-		name: params[:name], 
-		age: params[:age].to_i, 
-		city: params[:city]
-	)
-
-	if user.valid?
-	  user.save
-	  render json: user
-	else
-	  render json: {error: 'User could not be created.'}
+	def index
+		render json: User.all
 	end
-  end
+
+	def create
+		user = User.new(
+			username: params[:username],
+			password: params[:password], 
+			name: params[:name], 
+			age: params[:age].to_i, 
+			city: params[:city]
+		)
+
+		if user.valid?
+			user.save
+			render json: user
+		else
+			render json: {error: 'User could not be created.'}
+		end
+	end
+
+	def show
+		user = User.find(params[:id])
+		
+		if user.valid?
+			render json: user.attended_events.to_json(include: :game)
+		else
+			render json: {error: 'User does not have any hosted events.'}
+		end
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
 
-    resources :users, only: [:index, :create]
+    resources :users, only: [:index, :create, :show]
+    # get '/users/:id/hosted_events', to: 'users#hosted'
+    # get '/users/:id/attended_events', to: 'users#attended' 
+    
     resources :events, only: [:index, :show]
     
     post '/auth', to: 'auth#create'
